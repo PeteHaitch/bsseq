0641b0424581900a0d090950d2eda5448509d375
Peter Hickey <peter.hickey@gmail.com>
2016-06-15 16:25:38 -0400

    Remove remaining hdf5-kludges

--- R/getStats.R
+++ R/getStats.R
@@ -43,33 +43,127 @@ getStats_BSseqTstat <- function(BSseqTstat, regions = NULL, stat = "tstat.correc
     stopifnot(stat %in% colnames(BSseqTstat@stats))
     stopifnot(length(stat) == 1)
     stopifnot(is(regions, "GenomicRanges"))
+    # NOTE: Rather than split() ov, we split() the required columns of the
+    #       @stat slot. This is slightly more efficient if @stat is a matrix
+    #       and **much** more efficient if @stat is a DelayedArray
     ov <- findOverlaps(BSseqTstat, regions)
-    ov.sp <- split(queryHits(ov), subjectHits(ov))
-    getRegionStats <- function(idx) {
-        mat <- BSseqTstat@stats[idx,, drop=FALSE]
-        areaStat <- sum(mat[, stat])
-        maxStat <- max(mat[, stat])
+    mat <- BSseqTstat@stats[queryHits(ov), ]
+    mat_stat <- mat[, stat]
+    # NOTE: split,DelayedArray-method returns a *List and thus realises the
+    #       data in memory. And of course, split.array() returns a list, which
+    #       is already realised in memory
+    mat_stat_split <- split(mat_stat, subjectHits(ov))
+    getRegionStats <- function(stat) {
+        areaStat <- sum(stat)
+        maxStat <- max(stat)
         c(areaStat, maxStat)
     }
     stats <- matrix(NA, ncol = 2, nrow = length(regions))
     colnames(stats) <- c("areaStat", "maxStat")
-    tmp <- lapply(ov.sp, getRegionStats)
+    tmp <- lapply(mat_stat_split, getRegionStats)
     stats[as.integer(names(tmp)),] <- do.call(rbind, tmp)
     out <- as.data.frame(stats)
     if(! stat %in% c("tstat.corrected", "tstat"))
         return(out)
-    getRegionStats_ttest <- function(idx) {
-        mat <- BSseqTstat@stats[idx,, drop=FALSE]
-        group1.mean <- mean(mat[, "group1.means"])
-        group2.mean <- mean(mat[, "group2.means"])
-        meanDiff <- mean(mat[, "group1.means"] - mat[, "group2.means"])
-        tstat.sd <- mean(mat[, "tstat.sd"])
+    mat_group1.means <- BSseqTstat@stats[queryHits(ov), "group1.means"]
+    mat_group1.means_split <- split(mat_group1.means, subjectHits(ov))
+    mat_group2.means <- BSseqTstat@stats[queryHits(ov), "group2.means"]
+    mat_group2.means_split <- split(mat_group2.means, subjectHits(ov))
+    mat_tstat.sd <- BSseqTstat@stats[queryHits(ov), "tstat.sd"]
+    mat_tstat.sd_split <- split(mat_tstat.sd, subjectHits(ov))
+    getRegionStats_ttest <- function(g1m, g2m, tsd) {
+        group1.mean <- mean(g1m)
+        group2.mean <- mean(g2m)
+        meanDiff <- mean(g1m - g2m)
+        tstat.sd <- mean(tsd)
         c(meanDiff, group1.mean, group2.mean, tstat.sd)
     }
-    stats_ttest<- matrix(NA, ncol = 4, nrow = length(regions))
+    stats_ttest <- matrix(NA, ncol = 4, nrow = length(regions))
     colnames(stats_ttest) <- c("meanDiff", "group1.mean", "group2.mean", "tstat.sd")
-    tmp <- lapply(ov.sp, getRegionStats_ttest)
-    stats_ttest[as.integer(names(tmp)),] <- do.call(rbind, tmp)
+    tmp <- mapply(getRegionStats_ttest,
+                  g1m = split(mat_group1.means, subjectHits(ov)),
+                  g2m = split(mat_group2.means, subjectHits(ov)),
+                  tsd = split(mat_tstat.sd, subjectHits(ov)),
+                  SIMPLIFY = FALSE)
+    stats_ttest[as.integer(names(tmp)), ] <- do.call(rbind, tmp)
     out <- cbind(out, as.data.frame(stats_ttest))
     out
+
+    # # BENCHMARKING
+    # # -------------------------------------------------------------------------
+    # # NOTE: Faster to extract stats and then split rather than split and
+    # #       then extract, especially when BSseqTstat@stats is a DelayedArray
+    # stats_hdf5 <- BSseqTstat@stats
+    # stats_array <- as.array(stats_hdf5)
+    #
+    # extract_then_split_array <- function() {
+    #     s <- stats_array[queryHits(ov), stat]
+    #     base::tapply(s, subjectHits(ov), function(x) {
+    #         c(sum(x), max(x))
+    #     }, simplify = FALSE)
+    # }
+    # extract_then_split_hdf5 <- function() {
+    #     s <- stats_hdf5[queryHits(ov), stat]
+    #     tapply(s, subjectHits(ov), function(x) {
+    #         c(sum(x), max(x))
+    #     }, simplify = FALSE)
+    # }
+    # extract_then_split_array_old <- function() {
+    #     lapply(split(stats_array[queryHits(ov), stat], subjectHits(ov)),
+    #            function(x) {
+    #                areaStat <- sum(x)
+    #                maxStat <- max(x)
+    #                c(areaStat, maxStat)
+    #            })
+    # }
+    # extract_then_split_hdf5_realised <- function() {
+    #     s <- as.array(stats_hdf5[queryHits(ov), stat])
+    #     tapply(s, subjectHits(ov), function(x) {
+    #         c(sum(x), max(x))
+    #     }, simplify = FALSE)
+    # }
+    #
+    # extract_then_split_hdf5_old <- function() {
+    #     lapply(split(stats_hdf5[queryHits(ov), stat], subjectHits(ov)),
+    #            function(x) {
+    #                areaStat <- sum(x)
+    #                maxStat <- max(x)
+    #                c(areaStat, maxStat)
+    #            })
+    # }
+    # all.equal(extract_then_split_array(), as.list(extract_then_split_hdf5()))
+    # all.equal(extract_then_split_array(), extract_then_split_hdf5_realised())
+    # all.equal(extract_then_split_array(), extract_then_split_array_old())
+    # all.equal(extract_then_split_array(), as.list(extract_then_split_hdf5_old()))
+    #
+    #
+    # split_then_extract_array <- function() {
+    #     lapply(split(queryHits(ov), subjectHits(ov)), function(idx, stats) {
+    #         mat <- stats[idx,, drop=FALSE]
+    #         areaStat <- sum(mat[, stat])
+    #         maxStat <- max(mat[, stat])
+    #         c(areaStat, maxStat)
+    #     }, stats = stats_array)
+    # }
+    # split_then_extract_hdf5 <- function() {
+    #     lapply(split(queryHits(ov), subjectHits(ov)), function(idx, stats) {
+    #         mat <- stats[idx,, drop=FALSE]
+    #         areaStat <- sum(mat[, stat])
+    #         maxStat <- max(mat[, stat])
+    #         c(areaStat, maxStat)
+    #     }, stats = stats_hdf5)
+    # }
+    # all.equal(extract_then_split_array(), split_then_extract_array())
+    # all.equal(extract_then_split_array(), as.list(split_then_extract_hdf5()))
+    #
+    #
+    # # NOTE: Not testing split_then_extract_hdf5() because it's so slow
+    # microbenchmark::microbenchmark(extract_then_split_array(),
+    #                                extract_then_split_array_old(),
+    #                                extract_then_split_hdf5(),
+    #                                extract_then_split_hdf5_old(),
+    #                                extract_then_split_hdf5_realised(),
+    #                                split_then_extract_array(),
+    #                                times = 10)
+    # # -------------------------------------------------------------------------
 }
