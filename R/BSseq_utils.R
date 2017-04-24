8b7df6dd4cd03b0136849a698e41bb2a7e50eab9
Peter Hickey <peter.hickey@gmail.com>
2017-04-24 16:08:28 -0400

    Changes to getMeth() and getCoverage() - Add mc.cores arg (for use with non-NULL regions) - Simplify code

--- R/BSseq_utils.R
+++ R/BSseq_utils.R
@@ -54,148 +54,125 @@ orderBSseq <- function(BSseq, seqOrder = NULL) {
     BSseq[order(granges(BSseq))]
 }
 
-
+# TODO: Add BACKEND argument?
 getMeth <- function(BSseq, regions = NULL, type = c("smooth", "raw"),
-                    what = c("perBase", "perRegion"), confint = FALSE, alpha = 0.95) {
-    p.conf <- function(p, n, alpha) {
-        z <- abs(qnorm((1 - alpha)/2, mean = 0, sd = 1))
-        upper <- (p + z^2/(2*n) + z*sqrt(  (p*(1-p) + z^2/(4*n)) / n)) /
-            (1+z^2/n)
-        lower <- (p + z^2/(2*n) - z*sqrt(  (p*(1-p) + z^2/(4*n)) / n)) /
-            (1+z^2/n)
-        return(list(meth = p, lower = lower, upper = upper))
-    }
+                    what = c("perBase", "perRegion"), confint = FALSE,
+                    alpha = 0.95, mc.cores = 1) {
     stopifnot(is(BSseq, "BSseq"))
     type <- match.arg(type)
-    if(type == "smooth" & !hasBeenSmoothed(BSseq))
-        stop("'type=smooth' requires the object to have been smoothed.")
+    if (type == "smooth" & !hasBeenSmoothed(BSseq)) {
+        stop("'type = smooth' requires the object to have been smoothed.")
+    }
     what <- match.arg(what)
-    z <- abs(qnorm((1 - alpha)/2, mean = 0, sd = 1))
-    if(is.null(regions) && type == "smooth") {
-        meth <- getBSseq(BSseq, type = "trans")(getBSseq(BSseq, type = "coef"))
-        if(confint) {
-            upper <- getBSseq(BSseq, type = "trans")(getBSseq(BSseq, type = "coef") +
-                                      z * getBSseq(BSseq, type = "se.coef"))
-            lower <- getBSseq(BSseq, type = "trans")(getBSseq(BSseq, type = "coef") -
-                                      z * getBSseq(BSseq, type = "se.coef"))
-            return(list(meth = meth, lower = lower, upper = upper))
-        } else {
-            return(meth)
-        }
-    }
-    if(is.null(regions) && type == "raw") {
+
+    # NOTE: Computing meth is a delayed operation, so it is essentially free to
+    #       do this regardless of the size of the BSseq object
+    trans <- getBSseq(BSseq, type = "trans")
+    if (type == "smooth") {
+        meth <- trans(getBSseq(BSseq, type = "coef"))
+    } else if (type == "raw") {
         meth <- getBSseq(BSseq, type = "M") / getBSseq(BSseq, type = "Cov")
-        if(confint) {
-            return(p.conf(meth, n = getBSseq(BSseq, type = "Cov"), alpha = alpha))
-        } else {
+    }
+
+    if (is.null(regions)) {
+        if (!confint) {
             return(meth)
         }
+        z <- abs(qnorm((1 - alpha) / 2, mean = 0, sd = 1))
+        if (type == "smooth") {
+            upper <- trans(getBSseq(BSseq, type = "coef") +
+                               z * getBSseq(BSseq, type = "se.coef"))
+            lower <- trans(getBSseq(BSseq, type = "coef") -
+                               z * getBSseq(BSseq, type = "se.coef"))
+            return(list(meth = meth, lower = lower, upper = upper))
+        } else if (type == "raw")
+            # Function to compute confidence interval for a binomial
+            # probability (p)
+            p.conf <- function(p, n, alpha) {
+                z <- abs(qnorm((1 - alpha) / 2, mean = 0, sd = 1))
+                upper <- (p + z ^ 2 / (2 * n) + z *
+                              sqrt((p * (1 - p) + z ^ 2 / (4 * n)) / n)) /
+                    (1 + z ^ 2 / n)
+                lower <- (p + z ^ 2 / (2 * n) - z *
+                              sqrt((p * (1 - p) + z ^ 2 / (4 * n)) / n)) /
+                    (1 + z ^ 2 / n)
+                return(list(meth = p, lower = lower, upper = upper))
+            }
+        return(p.conf(p = meth,
+                      n = getBSseq(BSseq, type = "Cov"),
+                      alpha = alpha))
     }
-    ## At this point, regions have been specified
-    if(class(regions) == "data.frame")
+
+    # At this point, regions have been specified
+    if (class(regions) == "data.frame") {
         regions <- data.frame2GRanges(regions)
-    stopifnot(is(regions, "GenomicRanges"))
-    if(confint) stop("'confint = TRUE' is not supported by 'getMeth' when regions is given")
-    grBSseq <- granges(BSseq)
-    mm <- as.matrix(findOverlaps(regions, grBSseq))
-    mmsplit <- split(mm[,2], mm[,1])
-    if(what == "perBase") {
-        if(type == "smooth") {
-            out <- lapply(mmsplit, function(xx) {
-                getBSseq(BSseq, "trans")(getBSseq(BSseq, "coef")[xx,,drop = FALSE])
-            })
-        }
-        if(type == "raw") {
-            out <- lapply(mmsplit, function(xx) {
-                getBSseq(BSseq, "M")[xx,,drop = FALSE] / getBSseq(BSseq, "Cov")[xx,,drop = FALSE]
-            })
-        }
-        outList <- vector("list", length(regions))
-        outList[as.integer(names(mmsplit))] <- out
-        return(outList)
     }
-    if(what == "perRegion") {
-        if(type == "smooth") {
-            out <- lapply(mmsplit, function(xx) {
-                colMeans(getBSseq(BSseq, "trans")(getBSseq(BSseq, "coef")[xx,,drop = FALSE]), na.rm = TRUE)
-            })
-        }
-        if(type == "raw") {
-            out <- lapply(mmsplit, function(xx) {
-                colMeans(getBSseq(BSseq, "M")[xx,,drop = FALSE] / getBSseq(BSseq, "Cov")[xx,,drop = FALSE], na.rm = TRUE)
-            })
-        }
+    stopifnot(is(regions, "GenomicRanges"))
+    if (confint) {
+        stop("'confint = TRUE' is not supported by 'getMeth' when regions ",
+             "is given")
+    }
+    list_of_hits <- as(findOverlaps(regions, BSseq), "List")
+    if (what == "perBase") {
+        out <- mclapply(list_of_hits, function(i) {
+            meth[i, ]
+        }, mc.cores = mc.cores)
+        return(out)
+    } else if (what == "perRegion") {
+        out <- mclapply(list_of_hits, function(i) {
+            colMeans(meth[i, ], na.rm = TRUE)
+        }, mc.cores = mc.cores)
+
         out <- do.call(rbind, out)
-        outMatrix <- matrix(NA, ncol = ncol(BSseq), nrow = length(regions))
-        colnames(outMatrix) <- sampleNames(BSseq)
-        outMatrix[as.integer(rownames(out)),] <- out
-        .DelayedMatrix(outMatrix)
+        colnames(out) <- sampleNames(BSseq)
+        .DelayedMatrix(out)
     }
 }
 
+# TODO: Add BACKEND argument?
 getCoverage <- function(BSseq, regions = NULL, type = c("Cov", "M"),
-                    what = c("perBase", "perRegionAverage", "perRegionTotal")) {
+                        what = c("perBase", "perRegionAverage",
+                                 "perRegionTotal"),
+                        mc.cores = 1) {
     stopifnot(is(BSseq, "BSseq"))
     type <- match.arg(type)
     what <- match.arg(what)
-    if(is.null(regions)) {
-        if(what == "perBase")
-            return(getBSseq(BSseq, type = type))
-        if(what == "perRegionTotal")
-            return(colSums(getBSseq(BSseq, type = type)))
-        if(what == "perRegionAverage")
-            return(colMeans(getBSseq(BSseq, type = type)))
+
+    out <- getBSseq(BSseq, type = type)
+    if (is.null(regions)) {
+        if (what == "perBase") {
+            return(out)
+        }
+        if (what == "perRegionTotal") {
+            return(colSums(out))
+        }
+        if (what == "perRegionAverage") {
+            return(colMeans(out))
+        }
     }
-    if(class(regions) == "data.frame")
+
+    # At this point, regions have been specified
+    if (class(regions) == "data.frame") {
         regions <- data.frame2GRanges(regions)
+    }
     stopifnot(is(regions, "GenomicRanges"))
-    grBSseq <- granges(BSseq)
-    mm <- as.matrix(findOverlaps(regions, grBSseq))
-    mmsplit <- split(mm[,2], mm[,1])
-    if(what == "perBase") {
-        if(type == "Cov") {
-            out <- lapply(mmsplit, function(xx) {
-                getBSseq(BSseq, "Cov")[xx,,drop = FALSE]
-            })
-        }
-        if(type == "M") {
-            out <- lapply(mmsplit, function(xx) {
-                getBSseq(BSseq, "M")[xx,,drop = FALSE]
-            })
-        }
-        outList <- vector("list", length(regions))
-        outList[as.integer(names(mmsplit))] <- out
-        return(outList)
-    }
-    if(what == "perRegionAverage") {
-        if(type == "Cov") {
-            out <- lapply(mmsplit, function(xx) {
-                colMeans(getBSseq(BSseq, "Cov")[xx,,drop = FALSE], na.rm = TRUE)
-            })
-        }
-        if(type == "M") {
-            out <- lapply(mmsplit, function(xx) {
-                colMeans(getBSseq(BSseq, "M")[xx,,drop = FALSE], na.rm = TRUE)
-            })
-        }
-    }
-    if(what == "perRegionTotal") {
-        if(type == "Cov") {
-            out <- lapply(mmsplit, function(xx) {
-                colSums(getBSseq(BSseq, "Cov")[xx,,drop = FALSE], na.rm = TRUE)
-            })
-        }
-        if(type == "M") {
-            out <- lapply(mmsplit, function(xx) {
-                colSums(getBSseq(BSseq, "M")[xx,,drop = FALSE], na.rm = TRUE)
-            })
-        }
+
+    list_of_hits <- as(findOverlaps(regions, BSseq), "List")
+    if (what == "perBase") {
+        out <- mclapply(list_of_hits, function(i) {
+            out[i, ]
+        }, mc.cores = mc.cores)
+        return(out)
+    } else if (what == "perRegionAverage") {
+        out <- mclapply(list_of_hits, function(i) {
+            colMeans(out[i, ])
+        }, mc.cores = mc.cores)
+    } else if (what == "perRegionTotal") {
+        out <- mclapply(list_of_hits, function(i) {
+            colSums(out[i, ])
+        }, mc.cores = mc.cores)
     }
     out <- do.call(rbind, out)
-    outMatrix <- matrix(NA, ncol = ncol(BSseq), nrow = length(regions))
-    colnames(outMatrix) <- sampleNames(BSseq)
-    outMatrix[as.integer(rownames(out)),] <- out
-    .DelayedMatrix(outMatrix)
+    colnames(out) <- sampleNames(BSseq)
+    .DelayedMatrix(out)
 }
-
-
